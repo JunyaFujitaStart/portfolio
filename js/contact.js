3cf3c7b02353523ce0cd6ce3c1d40b002b77a123
“JunyaFujitaStart” <usausa13108891@gmail.com>
2022-03-24 07:31:30 +0000

    contact

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $('#form').submit(function(event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeV9vHGMReyGn1BnuAvWEPPxIEcl1Z5mkd95hZcYGV71fQCpQ/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function() {
+          $(".end-message").slideDown();
+          $(".button").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function() {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
